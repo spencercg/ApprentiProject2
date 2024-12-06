4afb6507899b892c8765490d3e7722921c08b5b1
spencercg <s.grimes.nv@gmail.com>
2024-12-06 12:08:23 -0800

    Commiting variables.tf file

---
+++ variables.tf
@@ -0,0 +1,16 @@
+variable "resource_group_name" {
+  type	= string
+  default = "sgrimesProject2RG"
+}
+
+variable "resource_group_location" {
+  type	= string
+  default = "East US"
+
+}
+
+variable "prefix" {
+	type = string
+	default = "sgrimesProject"
+
+}
