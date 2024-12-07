5b555af5b8084e50b460aa320518799e4fa54eda
spencercg <s.grimes.nv@gmail.com>
2024-12-06 16:17:44 -0800

    Adding VMSS config

--- main.tf
+++ main.tf
@@ -45,6 +45,54 @@ resource "azurerm_subnet" "storageSubnetSubnet" {
 
 
 
+resource "azurerm_public_ip" "pip" {
+  name                = "${var.prefix}-pip"
+  location            = var.resource_group_location
+  resource_group_name = var.resource_group_name
+  allocation_method   = "Static"
+  domain_name_label   = "spencercg92702"
+
+  tags = {
+    environment = "test"
+  }
+}
+
+resource "azurerm_lb" "loadblanacer" {
+  name                = "${var.prefix}-lb"
+  location            = var.resource_group_location
+  resource_group_name = var.resource_group_name
+
+  frontend_ip_configuration {
+    name                 = "PublicIPAddress"
+    public_ip_address_id = azurerm_public_ip.pip.id
+  }
+}
+
+resource "azurerm_lb_backend_address_pool" "backendpool" {
+  loadbalancer_id = azurerm_lb.loadblanacer.id
+  name            = "BackEndAddressPool"
+}
+
+resource "azurerm_lb_nat_pool" "lbnatpool" {
+  resource_group_name            = var.resource_group_name
+  name                           = "ssh"
+  loadbalancer_id                = azurerm_lb.loadblanacer.id
+  protocol                       = "Tcp"
+  frontend_port_start            = 50000
+  frontend_port_end              = 50119
+  backend_port                   = 22
+  frontend_ip_configuration_name = "PublicIPAddress"
+}
+
+resource "azurerm_lb_probe" "example" {
+  loadbalancer_id = azurerm_lb.loadblanacer.id
+  name            = "http-probe"
+  protocol        = "Http"
+  request_path    = "/health"
+  port            = 8080
+}
+
+
 resource "azurerm_network_interface" "sgrimesProjectNIC" {
   name                = "${var.prefix}-nic"
   location            = var.resource_group_location
@@ -59,39 +107,81 @@ resource "azurerm_network_interface" "sgrimesProjectNIC" {
 }
 
 
-resource "azurerm_virtual_machine" "webVM001" {
-  name                  = "${var.prefix}-webVM001"
-  location              = var.resource_group_location
-  resource_group_name   = var.resource_group_name
-  network_interface_ids = [azurerm_network_interface.sgrimesProjectNIC.id]
-  vm_size               = "Standard_d2_v3"
+resource "azurerm_virtual_machine_scale_set" "example" {
+  name                = "${var.prefix}-vmss"
+  location            = var.resource_group_location
+  resource_group_name = var.resource_group_name
 
-  # Uncomment this line to delete the OS disk automatically when deleting the VM
-  delete_os_disk_on_termination = true
+  # automatic rolling upgrade
+  automatic_os_upgrade = true
+  upgrade_policy_mode  = "Rolling"
 
-  # Uncomment this line to delete the data disks automatically when deleting the VM
-  delete_data_disks_on_termination = true
+  rolling_upgrade_policy {
+    max_batch_instance_percent              = 20
+    max_unhealthy_instance_percent          = 20
+    max_unhealthy_upgraded_instance_percent = 5
+    pause_time_between_batches              = "PT0S"
+  }
 
-  storage_image_reference {
+  # required when using rolling upgrade policy
+  health_probe_id = azurerm_lb_probe.example.id
+
+  sku {
+    name     = "Standard_F2"
+    tier     = "Standard"
+    capacity = 2
+  }
+
+  storage_profile_image_reference {
     publisher = "Canonical"
     offer     = "0001-com-ubuntu-server-jammy"
     sku       = "22_04-lts"
     version   = "latest"
   }
-  storage_os_disk {
-    name              = "myosdisk1"
+
+  storage_profile_os_disk {
+    name              = ""
     caching           = "ReadWrite"
     create_option     = "FromImage"
     managed_disk_type = "Standard_LRS"
   }
-  os_profile {
-    computer_name  = "hostname"
-    admin_username = "testadmin"
-    admin_password = "Password1234!"
+
+  storage_profile_data_disk {
+    lun           = 0
+    caching       = "ReadWrite"
+    create_option = "Empty"
+    disk_size_gb  = 10
   }
+
+  os_profile {
+    computer_name_prefix = "testvm"
+    admin_username       = ""
+    admin_password       = ""
+  }
+
   os_profile_linux_config {
     disable_password_authentication = false
+    /*
+    ssh_keys {
+      path     = "/home/myadmin/.ssh/authorized_keys"
+      key_data = file("~/.ssh/demo_key.pub")
+    }
+    */
   }
+
+  network_profile {
+    name    = "terraformnetworkprofile"
+    primary = true
+
+    ip_configuration {
+      name                                   = "TestIPConfiguration"
+      primary                                = true
+      subnet_id                              = azurerm_subnet.webSubnet.id
+      load_balancer_backend_address_pool_ids = [azurerm_lb_backend_address_pool.backendpool.id]
+      load_balancer_inbound_nat_rules_ids    = [azurerm_lb_nat_pool.lbnatpool.id]
+    }
+  }
+
   tags = {
     environment = "staging"
   }
