1063041795b47fc05d076d3bba42a2f4c2ee94fb
spencercg <s.grimes.nv@gmail.com>
2024-12-09 16:02:50 -0800

    Working on LB

--- main.tf
+++ main.tf
@@ -43,6 +43,63 @@ resource "azurerm_subnet" "storageSubnetSubnet" {
 }
 
 
+resource "azurerm_resource_group" "example" {
+  name     = "example-resources"
+  location = "West Europe"
+}
+
+resource "azurerm_network_security_group" "example" {
+  name                = "webSubnetNSG"
+  location            = var.resource_group_location
+  resource_group_name = var.resource_group_name
+  security_rule {
+    name                       = "InboundHTTPS"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "443"
+    destination_port_range     = "443"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+  security_rule {
+    name                       = "InboundICMP"
+    priority                   = 101
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Icmp"
+    source_port_range          = "*"
+    destination_port_range     = "*"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  security_rule {
+    name                       = "InboundHTTP"
+    priority                   = 102
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "80"
+    destination_port_range     = "80"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+
+
+  tags = {
+    environment = "testing"
+  }
+}
+
+
+
+resource "azurerm_subnet_network_security_group_association" "example" {
+  subnet_id                 = azurerm_subnet.webSubnet.id
+  network_security_group_id = azurerm_network_security_group.example.id
+}
 
 
 resource "azurerm_public_ip" "pip" {
@@ -51,28 +108,36 @@ resource "azurerm_public_ip" "pip" {
   resource_group_name = var.resource_group_name
   allocation_method   = "Static"
   domain_name_label   = "spencercg92702"
+  
 
   tags = {
     environment = "test"
   }
 }
 
-resource "azurerm_lb" "loadblanacer" {
+
+resource "azurerm_lb" "loadbalanacer" {
   name                = "${var.prefix}-lb"
   location            = var.resource_group_location
   resource_group_name = var.resource_group_name
+  
 
   frontend_ip_configuration {
     name                 = "PublicIPAddress"
     public_ip_address_id = azurerm_public_ip.pip.id
+    # subnet_id            = azurerm_subnet.webSubnet.id
   }
 }
 
 resource "azurerm_lb_backend_address_pool" "backendpool" {
-  loadbalancer_id = azurerm_lb.loadblanacer.id
+  loadbalancer_id = azurerm_lb.loadbalanacer.id
   name            = "BackEndAddressPool"
 }
 
+
+
+
+/*
 resource "azurerm_lb_nat_pool" "lbnatpool" {
   resource_group_name            = var.resource_group_name
   name                           = "ssh"
@@ -83,13 +148,28 @@ resource "azurerm_lb_nat_pool" "lbnatpool" {
   backend_port                   = 22
   frontend_ip_configuration_name = "PublicIPAddress"
 }
+*/
+
+
+resource "azurerm_lb_rule" "lbnatrule" {
+  loadbalancer_id                = azurerm_lb.loadbalanacer.id
+  name                           = "http"
+  protocol                       = "Tcp"
+  frontend_port                  = 80
+  backend_port                   = 80
+  backend_address_pool_ids       = [azurerm_lb_backend_address_pool.backendpool.id]
+  frontend_ip_configuration_name = azurerm_lb.loadbalanacer.frontend_ip_configuration[0].name
+  probe_id                       = azurerm_lb_probe.example.id
+}
+
+
 
 resource "azurerm_lb_probe" "example" {
-  loadbalancer_id = azurerm_lb.loadblanacer.id
+  loadbalancer_id = azurerm_lb.loadbalanacer.id
   name            = "http-probe"
   protocol        = "Http"
   request_path    = "/health"
-  port            = 8080
+  port            = 80
 }
 
 
@@ -107,15 +187,53 @@ resource "azurerm_network_interface" "sgrimesProjectNIC" {
 }
 
 
+
+
+
+
+resource "azurerm_linux_virtual_machine" "example" {
+  name                = "example-machine"
+  resource_group_name = var.resource_group_name
+  location            = var.resource_group_location
+  size                = "standard_d2_v3"
+  admin_username      = "adminuser"
+  network_interface_ids = [
+    azurerm_network_interface.sgrimesProjectNIC.id,
+  ]
+
+  admin_ssh_key {
+    username   = "adminuser"
+    public_key = file("~/.ssh/id_rsa.pub")
+  }
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-jammy"
+    sku       = "22_04-lts"
+    version   = "latest"
+  }
+}
+
+
+
+
+/*
+
 resource "azurerm_virtual_machine_scale_set" "example" {
   name                = "${var.prefix}-vmss"
   location            = var.resource_group_location
   resource_group_name = var.resource_group_name
 
   # automatic rolling upgrade
-  automatic_os_upgrade = true
-  upgrade_policy_mode  = "Rolling"
+  automatic_os_upgrade = false
+  upgrade_policy_mode  = "Automatic"
 
+/*
   rolling_upgrade_policy {
     max_batch_instance_percent              = 20
     max_unhealthy_instance_percent          = 20
@@ -123,8 +241,9 @@ resource "azurerm_virtual_machine_scale_set" "example" {
     pause_time_between_batches              = "PT0S"
   }
 
+
   # required when using rolling upgrade policy
-  health_probe_id = azurerm_lb_probe.example.id
+  # health_probe_id = azurerm_lb_probe.example.id
 
   sku {
     name     = "Standard_F2"
@@ -156,8 +275,7 @@ resource "azurerm_virtual_machine_scale_set" "example" {
   os_profile {
     computer_name_prefix = "testvm"
     admin_username       = ""
-    admin_password       = ""
-  }
+    admin_password       = "
 
   os_profile_linux_config {
     disable_password_authentication = false
@@ -166,7 +284,7 @@ resource "azurerm_virtual_machine_scale_set" "example" {
       path     = "/home/myadmin/.ssh/authorized_keys"
       key_data = file("~/.ssh/demo_key.pub")
     }
-    */
+    
   }
 
   network_profile {
@@ -177,8 +295,8 @@ resource "azurerm_virtual_machine_scale_set" "example" {
       name                                   = "TestIPConfiguration"
       primary                                = true
       subnet_id                              = azurerm_subnet.webSubnet.id
-      load_balancer_backend_address_pool_ids = [azurerm_lb_backend_address_pool.backendpool.id]
-      load_balancer_inbound_nat_rules_ids    = [azurerm_lb_nat_pool.lbnatpool.id]
+      # load_balancer_backend_address_pool_ids = [azurerm_lb_backend_address_pool.backendpool.id]
+      # load_balancer_inbound_nat_rules_ids    = [azurerm_lb_rule.lbnatrule.id]
     }
   }
 
@@ -186,3 +304,16 @@ resource "azurerm_virtual_machine_scale_set" "example" {
     environment = "staging"
   }
 }
+
+
+*/
+
+resource "azurerm_network_interface_backend_address_pool_association" "example" {
+  network_interface_id    = azurerm_network_interface.sgrimesProjectNIC.id
+  ip_configuration_name   = "testconfiguration1"
+  backend_address_pool_id = azurerm_lb_backend_address_pool.backendpool.id
+}
+
+
+
+
