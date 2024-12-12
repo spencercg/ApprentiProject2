f7b13d461ce4229e3b7731570fa859a86b02f3a8
spencercg <s.grimes.nv@gmail.com>
2024-12-11 19:45:34 -0800

    Edited the db firewall to just web subnet

--- database.tf
+++ database.tf
@@ -1,9 +1,9 @@
 resource "azurerm_mssql_server" "example" {
-  name                = "sgrimesmssqlserver"
-  resource_group_name = var.resource_group_name
-  location            = var.resource_group_location
-  version             = "12.0"
-  administrator_login = ""
+  name                         = "sgrimesmssqlserver"
+  resource_group_name          = var.resource_group_name
+  location                     = var.resource_group_location
+  version                      = "12.0"
+  administrator_login          = ""
   administrator_login_password = ""
 
 }
@@ -15,3 +15,23 @@ resource "azurerm_mssql_database" "example" {
 
 }
 
+resource "azurerm_private_endpoint" "example" {
+  name                = "example-endpoint"
+  location            = var.resource_group_location
+  resource_group_name = var.resource_group_name
+  subnet_id           = azurerm_subnet.dbSubnet.id
+
+  private_service_connection {
+    name                           = "example-service-connection"
+    is_manual_connection           = false
+    private_connection_resource_id = azurerm_mssql_server.example.id
+    subresource_names = ["sqlServer"]
+  }
+}
+
+resource "azurerm_mssql_firewall_rule" "example" {
+  name             = "AllowFromVNet"
+  server_id        = azurerm_mssql_server.example.id
+  start_ip_address = "10.0.1.0"
+  end_ip_address   = "10.0.1.255"
+}
